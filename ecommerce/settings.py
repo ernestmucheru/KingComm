4613180fb0dec017493eb21fdbbc842bb958a38b
Ernest <ernestmucheru254@gmail.com>
2022-03-02 17:28:52 +0300

    intergrated cloudinary to the project

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,9 +12,20 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config,Csv
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+cloudinary.config(
+    cloud_name = 'mmwangimucheru',
+    api_key = '374576266154276',
+    api_secret = 'JO6MbShOUPFIYs_FYcod3iYTBQA'
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
