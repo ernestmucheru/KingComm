02a3198c3753311372d136e8d04b9df482fe7eb6
Ernest <ernestmucheru254@gmail.com>
2022-03-02 18:50:34 +0300

    added product image field

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 import email
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Customer(models.Model):
@@ -15,6 +16,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
+    image = CloudinaryField('image', null=True, blank=True)
 
     def __str__(self):
         return self.name
