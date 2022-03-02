a661afcc4c94d4179d2c9866448223bc063c454e
Ernest <ernestmucheru254@gmail.com>
2022-03-02 16:19:24 +0300

    created the order model

--- store/models.py
+++ store/models.py
@@ -18,3 +18,12 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return str(self.id)
